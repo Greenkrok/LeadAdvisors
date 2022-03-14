1f30253d085c112ca08de5eef64210a1e8408516
Zharkevich Ilya <zharkevichilya@gmail.com>
2022-03-14 17:47:05 +0300

    feat: add ability to sending form

--- src/scripts/sendForm.js
+++ src/scripts/sendForm.js
@@ -2,13 +2,8 @@ const sendingBtn = document.querySelector('.email-form__btn');
 sendingBtn.onclick = (e) => {
   e.preventDefault();
   triggerModal();
+  sendForm();
 };
-
-const modalCross = document.querySelector('.popap__close');
-modalCross.onclick = () => triggerModal();
-
-const modalBtn = document.querySelector('.popap__btn');
-modalBtn.onclick = () => triggerModal();
 
 const triggerModal = () => {
   const modal = document.querySelector('.modal');
@@ -19,4 +14,50 @@ const triggerModal = () => {
     modal.classList.add('open');
   }
 
+}
+
+const modalCross = document.querySelector('.popap__close');
+modalCross.onclick = () => triggerModal();
+
+const modalBtn = document.querySelector('.popap__btn');
+modalBtn.onclick = () => triggerModal();
+
+function validateEmail(inputValue) {
+  const format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let isEmailValid;
+
+  inputValue.match(format) ? isEmailValid = true : isEmailValid = false;
+
+  return isEmailValid;
+}
+
+const sendForm = async () => {
+  const email = document.querySelector(".email-form__input");
+  const titleModal = document.querySelector(".popap__title");
+  const textModal = document.querySelector(".popap__text");
+  let isEmailValid = validateEmail(email.value);
+  const success = {
+    title: "SUCCESS!",
+    text: "You have successfully subscribed to the email newsletter"
+  };
+  const failing = {
+    title: "FAIL!",
+    text: `Something went wrong :(
+      Make sure you have entered your email correctly and try again`
+  };
+
+  if (isEmailValid) {
+    await fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then( () => {
+        titleModal.innerText = success.title;
+        textModal.innerText = success.text;
+      })
+      .catch(() => {
+        titleModal.innerText = failing.title;
+        textModal.innerText = failing.text;
+      });
+  } else {
+    titleModal.innerText = failing.title;
+    textModal.innerText = failing.text;  
+  }
 }
